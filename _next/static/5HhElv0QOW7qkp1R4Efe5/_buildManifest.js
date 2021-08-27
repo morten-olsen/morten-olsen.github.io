721d0eba7127ff2e7bc0774e97c8ddac187a8a7e
morten-olsen <github@buy-me.coffee>
2021-08-27 19:11:32 +0000

    Deploying to gh-pages from @ morten-olsen/morten-olsen.github.io@84f4de421fc6953e37234a9c0784775c509d7726 🚀

---
+++ _next/static/5HhElv0QOW7qkp1R4Efe5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-301084e25badb7798cb0.js","static/chunks/90-3940295255ee396cac3a.js","static/chunks/pages/index-d25911e8f54356216fa9.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
