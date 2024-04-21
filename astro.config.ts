b34cc2f1da118ead6baf23381fe5e9ff84ea2ae0
Morten Olsen <fbtijfdq@void.black>
2024-04-21 19:30:38 +0200

    minor update

--- astro.config.ts
+++ astro.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import icon from 'astro-icon'
-import compress from 'astro-compress'
 import robotsTxt from 'astro-robots-txt'
+import compress from '@playform/compress'
+
 const getSiteInfo = () => {
   const siteUrl = process.env.SITE_URL
   if (!siteUrl) {
