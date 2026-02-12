e0c3ddecdc114c3288a4cb7478511ca2131dab39
Morten Olsen <fbtijfdq@void.black>
2026-02-12 19:59:05 +0100

    improved images

--- astro.config.ts
+++ astro.config.ts
@@ -26,6 +26,7 @@ export default defineConfig({
   },
   integrations: [mdx(), sitemap(), icon(), compress({
     HTML: false,
+    Image: false,
   }), robotsTxt()],
   devToolbar: {
     enabled: false,
